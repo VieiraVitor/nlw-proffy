2e2dcc95e292271a078ebf0f7aa127891d8b992b
VieiraVitor <vieirachp@hotmail.com>
2020-08-08 16:39:21 -0300

    feat: adiciona rota para listar as conexões

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,9 +6,10 @@ const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
 
-routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
+routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
 
 export default routes;
